46a0c245d2ec34fd5cad626977f0547f6f49dcc4
Gabrielx47 <62863349+Gabrielx47@users.noreply.github.com>
2024-09-04 11:50:24 -0300

    Implementação de outras operações matemáticas.

--- app.py
+++ app.py
@@ -1,7 +1,24 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
 @app.get("/operations")
-def retornarOperacoesDisponiveis():
-    return jsonify(["Soma", "subtração", "Divisão", "Multiplicação"])
+def retornarOperacoesDisponiveis_get():
+    return jsonify(["Adição", "subtração", "Divisão", "Multiplicação"])
+
+
+@app.post("/operations/addition")
+def somar_post():
+    return jsonify(int(request.form["valor1"]) + int(request.form["valor2"]))
+
+@app.post("/operations/subtraction")
+def subtrair_post():
+    return jsonify(int(request.form["valor1"]) - int(request.form["valor2"]))
+
+@app.post("/operations/multiplication")
+def multiplicar_post():
+    return jsonify(int(request.form["valor1"]) * int(request.form["valor2"]))
+
+@app.post("/operations/division")
+def dividir_post():
+    return jsonify(int(request.form["valor1"]) / int(request.form["valor2"]))
