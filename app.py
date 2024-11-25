58e5394cb90e97206899deac82fcc2ae3db9efef
Gabrielx47 <62863349+Gabrielx47@users.noreply.github.com>
2024-11-24 22:01:41 -0300

    Criação da documentação da API.

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
 from flask import Flask, jsonify, request
+from flasgger import Swagger, swag_from
 
 app = Flask(__name__)
+app.config['SWAGGER'] = {
+    'title': 'Calculator',
+    'openapi': '3.0.0'
+}
 
-@app.get("/operations")
+swagger = Swagger(app, template_file="docs/openapi.yaml")
+
+@app.route("/operations", methods=["GET"])
 def retornarOperacoesDisponiveis_get():
     return jsonify(["Adição", "subtração", "Divisão", "Multiplicação"])
 
-
-@app.post("/operations/addition")
+@app.route("/operations/addition", methods=["POST"])
 def somar_post():
     return jsonify(int(request.form["valor1"]) + int(request.form["valor2"]))
 
